d689dcd612a61f90ffbf6611dd1e2c8e2128d90e
Tomoya <sendatomoya0616@gmail.com>
2025-04-18 21:17:06 +0900

    Initial commit

--- lib/message.dart
+++ lib/message.dart
@@ -25,6 +25,7 @@ class Message extends StatelessWidget {
                     ],
                   ),
                 );
+                return null;
               },
             )
           ],
